c54c782e1eae816ef848b7521843e3af1e9161ba
Rybak Maksym <maksym.rybak@sifex-tech.com>
2025-06-25 15:13:47 +0300

    add: pages url config

---
+++ src/shared/config/pages-url.config.ts
@@ -0,0 +1,12 @@
+class PAGES {
+  private root = '/';
+
+  REGISTER = `${this.root}register`;
+  LOG_IN = `${this.root}log-in`;
+
+  PROFILES = `${this.root}`;
+  USERS = `${this.root}users`;
+  DASHBOARD = `${this.root}dashboard`;
+}
+
+export const PAGES_URL = new PAGES();
