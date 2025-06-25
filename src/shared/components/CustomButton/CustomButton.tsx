2cbc348be8d373d44fade4f595fef250743d10bb
Rybak Maksym <maksym.rybak@sifex-tech.com>
2025-06-25 11:11:52 +0300

    add: custom button

---
+++ src/shared/components/CustomButton/CustomButton.tsx
@@ -0,0 +1,33 @@
+import { Button, ButtonProps } from '@mui/material';
+import { ReactNode } from 'react';
+
+type CustomButtonProps = ButtonProps & {
+  children: ReactNode;
+  customVariant?: 'red' | 'green';
+};
+
+function CustomButton({
+  children,
+  customVariant,
+  ...props
+}: CustomButtonProps) {
+  return (
+    <Button
+      sx={{
+        padding: '16px 94px 25px 94px',
+        fontFamily: 'Open Sans',
+        fontWeight: '700',
+        fontSize: '21px',
+        color: customVariant === 'green' ? '#12130F' : '#FFFFFF',
+        backgroundColor: customVariant === 'green' ? '#8FCB9B' : '#AF253C',
+        borderRadius: '20px',
+      }}
+      {...props}
+    >
+      {children}
+    </Button>
+  );
+}
+
+export default CustomButton;
+
