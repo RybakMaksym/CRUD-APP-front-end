b19694ed69e4674c0c5d22ccfa97cdc20f0da2bc
Rybak Maksym <maksym.rybak@sifex-tech.com>
2025-06-25 15:17:37 +0300

    add: formik use field hook to checkbox

--- src/shared/components/CustomCheckbox/CustomCheckbox.tsx
+++ src/shared/components/CustomCheckbox/CustomCheckbox.tsx
@@ -1,4 +1,5 @@
 import { Checkbox, CheckboxProps } from '@mui/material';
+import { useField } from 'formik';
 import Paragraph from '../Paragraph/Paragraph';
 
 type CustomCheckboxProps = CheckboxProps & {
@@ -6,25 +7,34 @@ type CustomCheckboxProps = CheckboxProps & {
 };
 
 function CustomCheckbox({ label, ...props }: CustomCheckboxProps) {
+  const [field, meta] = useField({ ...props, type: 'checkbox' });
+
   return (
-    <div style={{ display: 'flex', alignItems: 'center', gap: '24px' }}>
-      <Checkbox
-        sx={{
-          color: '#AF253C',
-          '&:hover': {
-            backgroundColor: '#fff2f5',
-          },
-          '&.Mui-checked': {
+    <>
+      <div style={{ display: 'flex', alignItems: 'center', gap: '24px' }}>
+        <Checkbox
+          sx={{
             color: '#AF253C',
-          },
-          '&.Mui-checked:hover': {
-            backgroundColor: '#fff2f5',
-          },
-        }}
-        {...props}
-      />
-      <Paragraph>{label}</Paragraph>
-    </div>
+            '&:hover': {
+              backgroundColor: '#fff2f5',
+              opacity: '0.1',
+            },
+            '&.Mui-checked': {
+              color: '#AF253C',
+            },
+            '&.Mui-checked:hover': {
+              backgroundColor: '#fff2f5',
+            },
+          }}
+          {...props}
+          {...field}
+        />
+        <Paragraph>{label}</Paragraph>
+      </div>
+      {meta.touched && meta.error ? (
+        <Paragraph variant="error">{meta.error}</Paragraph>
+      ) : null}
+    </>
   );
 }
 
