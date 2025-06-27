425218789b9230e9e33284a4fb7976d7983f1ca9
Rybak Maksym <maksym.rybak@sifex-tech.com>
2025-06-27 13:10:29 +0300

    fix: gloabal css import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import { Open_Sans } from 'next/font/google';
 
 import Providers from '@/app/Providers';
-import 'app/globals.css';
+import '@/app/globals.css';
 
 const openSans = Open_Sans({
   subsets: ['latin'],
