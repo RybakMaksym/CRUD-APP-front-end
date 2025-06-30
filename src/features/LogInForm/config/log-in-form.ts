5f22fa722e3c665a1ec07da58395e3a83c42255f
Rybak Maksym <maksym.rybak@sifex-tech.com>
2025-06-30 11:43:33 +0300

    add: auth schema

--- src/features/LogInForm/config/log-in-form.ts
+++ src/features/LogInForm/config/log-in-form.ts
@@ -1,15 +1,5 @@
-import * as Yup from 'yup';
+import { AUTH_SCHEMA } from '@/shared/schemas/auth-schema';
 
-export const LOG_IN_FORM_SCHEMA = Yup.object({
-  email: Yup.string().email('Invalid email').required('Required'),
-  password: Yup.string()
-    .required('Password is required')
-    .min(8, 'Password must be at least 8 characters')
-    .max(32, 'Password must be at most 32 characters')
-    .matches(/^[^\s'"`\\]+$/, {
-      message:
-        'Password must not contain spaces or invalid characters like quotes or backslashes',
-    }),
-});
+export const LOG_IN_FORM_SCHEMA = AUTH_SCHEMA;
 
 export const LOG_IN_FORM_DEFAULT_VALUES = { email: '', password: '' };
--- src/features/RegisterForm/config/register-form.ts
+++ src/features/RegisterForm/config/register-form.ts
@@ -1,18 +1,13 @@
 import * as Yup from 'yup';
 
-export const REGISTER_FORM_SCHEMA = Yup.object({
-  username: Yup.string().required('Required'),
-  email: Yup.string().email('Invalid email').required('Required'),
-  password: Yup.string()
-    .required('Password is required')
-    .min(8, 'Password must be at least 8 characters')
-    .max(32, 'Password must be at most 32 characters')
-    .matches(/^[^\s'"`\\]+$/, {
-      message:
-        'Password must not contain spaces or invalid characters like quotes or backslashes',
-    }),
-  isAdmin: Yup.boolean().default(false),
-});
+import { AUTH_SCHEMA } from '@/shared/schemas/auth-schema';
+
+export const REGISTER_FORM_SCHEMA = AUTH_SCHEMA.concat(
+  Yup.object({
+    username: Yup.string().required('Required'),
+    isAdmin: Yup.boolean().default(false),
+  }),
+);
 
 export const REGISTER_FORM_DEFAULT_VALUES = {
   username: '',
