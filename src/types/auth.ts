93ae522a46370a8d86fc123175c97ac177e56d65
Rybak Maksym <maksym.rybak@sifex-tech.com>
2025-06-30 17:06:31 +0300

    add: auth state type

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -15,3 +15,8 @@ export interface IAuthResponse {
   accessToken: string;
   refreshToken: string;
 }
+
+export interface AuthState {
+  accessToken: string | null;
+  refreshToken: string | null;
+}
