4acab390f7fd6790bf5be76e2db96048e6cd4b55
Rybak Maksym <maksym.rybak@sifex-tech.com>
2025-06-30 17:07:44 +0300

    add: store & auth api

---
+++ src/redux/auth/auth.api.ts
@@ -0,0 +1,48 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+import { RootState } from '@/redux/store';
+import { IAuthResponse, ILogInForm, IRegisterForm } from '@/types/auth';
+
+export const authApi = createApi({
+  reducerPath: 'authApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000',
+    credentials: 'include',
+    prepareHeaders: (headers, { getState }) => {
+      let token = (getState() as RootState).auth.accessToken;
+
+      if (!token) {
+        token = (getState() as RootState).auth.refreshToken;
+      }
+
+      if (token) headers.set('Authorization', `Bearer ${token}`);
+
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    register: builder.mutation<IAuthResponse, IRegisterForm>({
+      query: (body) => ({
+        url: '/auth/register',
+        method: 'POST',
+        body,
+      }),
+    }),
+    logIn: builder.mutation<IAuthResponse, ILogInForm>({
+      query: (body) => ({
+        url: '/auth/log-in',
+        method: 'POST',
+        body,
+      }),
+    }),
+    refreshTokens: builder.query<IAuthResponse, void>({
+      query: () => ({
+        url: '/token/refresh',
+        method: 'GET',
+      }),
+    }),
+  }),
+});
+
+export const { useRegisterMutation, useLogInMutation, useRefreshTokensQuery } =
+  authApi;
---
+++ src/redux/store.ts
@@ -0,0 +1,43 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  persistReducer,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+} from 'redux-persist';
+import persistStore from 'redux-persist/es/persistStore';
+import storage from 'redux-persist/lib/storage';
+
+import { authApi } from '@/redux/auth/auth.api';
+import { authReducer } from '@/redux/auth/auth.slice';
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+  [authApi.reducerPath]: authApi.reducer,
+});
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['auth'],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(authApi.middleware),
+});
+
+export const persistor = persistStore(store);
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
