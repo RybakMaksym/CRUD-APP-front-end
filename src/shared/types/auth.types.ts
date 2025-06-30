3c65f09d59ae57891bd2e74bf32b127390543458
Rybak Maksym <maksym.rybak@sifex-tech.com>
2025-06-30 12:17:01 +0300

    fix: IRegisterForm type

--- src/shared/types/auth.types.ts
+++ src/shared/types/auth.types.ts
@@ -1,15 +1,13 @@
 import { IUser } from '@/shared/types/user.types';
 
-export interface IRegisterForm {
-  username: string;
-  email: string;
-  password: string;
-  isAdmin: boolean;
-}
-
 export interface ILogInForm {
   email: string;
   password: string;
+}
+
+export interface IRegisterForm extends ILogInForm {
+  username: string;
+  isAdmin: boolean;
 }
 
 export interface IAuthResponse {
