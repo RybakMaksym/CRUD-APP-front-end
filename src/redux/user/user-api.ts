f69335bbc39b2f048beb21939351f19bf8da7b8f
Rybak Maksym <maksym.rybak@sifex-tech.com>
2025-07-11 09:20:27 +0300

    create: user api parameters types

--- src/redux/user/user-api.ts
+++ src/redux/user/user-api.ts
@@ -1,7 +1,9 @@
 import { createApi } from '@reduxjs/toolkit/query/react';
 
 import { baseQuery } from '@/redux/queries/base-query';
+import { UpdateUserByIdParams } from '@/redux/user/user-types';
 import { IMessageResponse } from '@/types/messages';
+import { IPagination, ISearch } from '@/types/navigation';
 import { IUser } from '@/types/user';
 
 export const userApi = createApi({
@@ -9,7 +11,7 @@ export const userApi = createApi({
   baseQuery,
   tagTypes: ['User'],
   endpoints: (builder) => ({
-    usersList: builder.query<IUser[], { page: number; limit: number }>({
+    usersList: builder.query<IUser[], IPagination>({
       query: ({ page, limit }) => ({
         url: `/user/list?page=${page}&limit=${limit}`,
         method: 'GET',
@@ -23,7 +25,7 @@ export const userApi = createApi({
       }),
       providesTags: [{ type: 'User', id: 'TOTAL' }],
     }),
-    searchUsers: builder.query<IUser[], { query: string }>({
+    searchUsers: builder.query<IUser[], ISearch>({
       query: ({ query }) => ({
         url: `/user/search?query=${query}`,
         method: 'GET',
@@ -36,19 +38,17 @@ export const userApi = createApi({
       }),
       providesTags: [{ type: 'User', id: 'ID' }],
     }),
-    updateUserById: builder.mutation<IUser, { id: string; formData: FormData }>(
-      {
-        query: ({ id, formData }) => ({
-          url: `/user/update/${id}`,
-          method: 'PATCH',
-          body: formData,
-        }),
-        invalidatesTags: () => [
-          { type: 'User', id: 'LIST' },
-          { type: 'User', id: 'ID' },
-        ],
-      },
-    ),
+    updateUserById: builder.mutation<IUser, UpdateUserByIdParams>({
+      query: ({ id, formData }) => ({
+        url: `/user/update/${id}`,
+        method: 'PATCH',
+        body: formData,
+      }),
+      invalidatesTags: () => [
+        { type: 'User', id: 'LIST' },
+        { type: 'User', id: 'ID' },
+      ],
+    }),
     deleteUserById: builder.mutation<IMessageResponse, string>({
       query: (id) => ({
         url: `/user/${id}`,
---
+++ src/redux/user/user-types.ts
@@ -0,0 +1,1 @@
+export type UpdateUserByIdParams = { id: string; formData: FormData };
