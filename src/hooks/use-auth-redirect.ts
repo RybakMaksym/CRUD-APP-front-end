0a066aacc6c30e73f425beadef4517d7bf34795f
RybakMaksym <maksym.rybak@sifex-tech.com>
2025-07-17 08:50:13 +0300

    add: auth routes guard

--- src/hooks/use-auth-redirect.ts
+++ src/hooks/use-auth-redirect.ts
@@ -1,10 +1,11 @@
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 
+import { PAGES_URL } from '@/enums/pages-url';
 import { useAppSelector } from '@/hooks/use-app-selector';
 import authSelectors from '@/redux/auth/auth-selectors';
 
-export function useAuthRedirect(redirectTo = '/log-in') {
+export function useAuthRedirect(redirectTo = PAGES_URL.LOG_IN) {
   const router = useRouter();
   const accessToken = useAppSelector(authSelectors.getAccessToken);
 
