7184f4128e1047fc9cbc28aba38d24d62de27c9e
Rybak Maksym <maksym.rybak@sifex-tech.com>
2025-07-29 16:06:03 +0300

    configure: jest

---
+++ jest.config.ts
@@ -0,0 +1,43 @@
+import type { Config } from 'jest';
+import nextJest from 'next/jest';
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const customJestConfig: Config = {
+  clearMocks: true,
+  collectCoverage: true,
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+  coverageProvider: 'v8',
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/**/__tests__/**',
+    '!src/**/types.ts',
+  ],
+  coveragePathIgnorePatterns: [
+    '<rootDir>/src/app/.*layout\\.tsx',
+    '<rootDir>/src/app/.*not-found\\.tsx',
+    '<rootDir>/src/app/.*page\\.tsx',
+    '<rootDir>/src/providers/ClientProviders\\.tsx',
+    '<rootDir>/src/providers/test-utils\\.tsx',
+    '<rootDir>/src/redux/store\\.ts',
+    '<rootDir>/src/enums',
+    '<rootDir>/src/types',
+    '<rootDir>/src/styles',
+    '<rootDir>/src/lib/constants',
+    '<rootDir>/src/hooks/use-app-.*\\.ts',
+    '<rootDir>/src/redux/queries',
+    '<rootDir>/src/redux/auth',
+    '<rootDir>/src/redux/user',
+    '<rootDir>/src/redux/profile',
+  ],
+};
+
+export default createJestConfig(customJestConfig);
