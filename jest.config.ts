256579d3971ea5cfe79efa30253efd9d21fdca17
Rybak Maksym <maksym.rybak@sifex-tech.com>
2025-07-30 16:36:21 +0300

    fix: jest config

--- jest.config.ts
+++ jest.config.ts
@@ -11,7 +11,7 @@ const customJestConfig: Config = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   testEnvironment: 'jsdom',
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/$1',
+    '^@/(.*)$': '<rootDir>/src/$1',
   },
   coverageProvider: 'v8',
   coverageDirectory: 'coverage',
